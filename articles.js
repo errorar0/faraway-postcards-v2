c5fa59ea59b614bdeccdc5d8248c790a224177df
errorar0 <errors.ar@gmail.com>
2026-05-28 17:42:48 +0700

    Create articles.js

---
+++ articles.js
@@ -0,0 +1,91 @@
+const articles = [
+
+{
+  title: "Amazing Desserts Around the World Almost Nobody Knows About",
+  url: "articles/amazing-desserts-around-the-world.html",
+  image: "images/amazing-desserts-around-the-world.jpeg",
+  country: "World",
+  experience: "Food"
+},
+
+{
+  title: "How France Changed the Taste of Vietnam",
+  url: "articles/how-france-changed-the-taste-of-vietnam.html",
+  image: "images/Vietnam-taste-french-hero.jpeg",
+  country: "Vietnam",
+  experience: "Food"
+},
+
+{
+  title: "Kazakhstan That Feels Like Another Planet",
+  url: "articles/kazakhstan-that-feels-like-another-planet.html",
+  image: "images/kazakhstan-hero.jpeg",
+  country: "Kazakhstan",
+  experience: "Guidebook"
+},
+
+{
+  title: "Locations in Mexico That Feel Too Surreal to Be Real",
+  url: "articles/locations-in-mexico-that-feel-too-surreal-to-be-real.html",
+  image: "images/mexico-surreal-cover.jpeg",
+  country: "Mexico",
+  experience: "Guidebook"
+},
+
+{
+  title: "Where Georgia Feels Endless",
+  url: "articles/where-georgia-feels-endless.html",
+  image: "images/georgia-where-georgia-feels-endless.jpg",
+  country: "Georgia",
+  experience: "Guidebook"
+},
+
+{
+  title: "The Weeks When Boats Stop Running",
+  url: "articles/philippines-boats-stop-running.html",
+  image: "images/philippines-boats-stop-running-cover.jpeg",
+  country: "Philippines",
+  experience: "Guidebook"
+},
+
+{
+  title: "The Geography of Flavor in Vietnam",
+  url: "articles/the-geography-of-flavor-in-vietnam.html",
+  image: "images/vietnam/vietnam-geography-of-flavor-hero.jpeg",
+  country: "Vietnam",
+  experience: "Food"
+},
+
+{
+  title: "The Philippines Most People Never Imagine",
+  url: "articles/the-philippines-most-people-never-imagine.html",
+  image: "images/philippines-volcanoes-cover.jpeg",
+  country: "Philippines",
+  experience: "Nature"
+},
+
+{
+  title: "Ten Days Through Vietnam’s Food Culture",
+  url: "articles/vietnam-food-10-days.html",
+  image: "images/vietnam-food-culture-cover.jpeg",
+  country: "Vietnam",
+  experience: "Food"
+},
+
+{
+  title: "The Volcanic Architecture of Armenia",
+  url: "articles/the-volcanic-architecture-of-armenia.html",
+  image: "images/yerevan_armenia.jpeg",
+  country: "Armenia",
+  experience: "Architecture"
+},
+
+{
+  title: "Places That Work Better as Photographs Than Real Experiences",
+  url: "articles/places-that-work-better-as-photographs.html",
+  image: "images/but-hero.jpeg",
+  country: "World",
+  experience: "Guidebook"
+}
+
+];
