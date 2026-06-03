53ca41b805649d6157685f61a3ad0eb33c42dcc9
errorar0 <errors.ar@gmail.com>
2026-06-03 07:15:23 +0700

    Create header-v3.js

---
+++ header-v3.js
@@ -0,0 +1,13 @@
+document.getElementById("header").innerHTML = `
+<header class="site-header">
+  <a href="../index-v3.html" class="logo">
+    Faraway <span>Postcards</span>
+  </a>
+
+  <nav>
+    <a href="../index-v3.html">Home</a>
+    <a href="../index-v3.html#latest">Articles</a>
+    <a href="../about.html">About</a>
+  </nav>
+</header>
+`;
